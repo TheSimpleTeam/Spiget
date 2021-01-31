11bba56645a74e1b70ff656e1fc76eb9223217bb
BlueTree242 <ae011d482207d463e261d6d56c1a3b44d6e0521c>
2021-01-31 18:44:12 +0200

    getRate in Rating.java

--- src/main/java/me/bluetree/spiget/Rating.java
+++ src/main/java/me/bluetree/spiget/Rating.java
@@ -1,19 +1,28 @@
 package me.bluetree.spiget;
 
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Rating {
     private int count;
-    private int average;
-    public Rating(int count , int averge){
+    private Long average;
+    public Rating(int count , Long averge){
         this.count = count;
-        this.average = count;
+        this.average = averge;
     }
 
-    public int getAverage() {
+    public Long getAverage() {
         return average;
     }
 
     public int getCount() {
         return count;
     }
+
+    public Float getRate() {
+        return BigDecimal.valueOf(average).setScale(1, RoundingMode.HALF_UP).floatValue();
+    }
+
+
 }
